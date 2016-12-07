53cbd0bb6c4900bef9e586ce530381adf2efa5d7
Matthew Woodhead <mwoodhead@ad.davenport.edu>
2016-12-07 16:31:23 -0500

    Added Default Project

---
+++ Gradiens/Gradiens/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Gradiens.MvcApplication" Language="C#" %>
---
+++ Gradiens/Gradiens/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Gradiens.Startup))]
+namespace Gradiens
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
