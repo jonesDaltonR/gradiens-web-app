656bd46d8ca079a6b126d8c769c0e1078911fa0d
Michael Trinh <mtrinh1@email.davenport.edu>
2016-12-29 20:37:19 -0500

    Fixed some mistakes in the database and created a base for all the Models.

---
+++ Gradiens/Gradiens/Models/BusModels.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Gradiens.Models
+{
+    // Bus object that is holds information for the BUS table
+    public class BusModels
+    {
+        [Required]
+        [Display(Name = "Bus ID")]
+        public long BUS_ID { get; set; }
+
+        [Required]
+        [Display(Name = "Bus Company Name")]
+        public string BUS_COMPANY { get; set; }
+
+        [Required]
+        [Display(Name = "Bus Stop ID")]
+        public long BUS_STOP_ID { get; set; }
+
+        [Required]
+        [Display(Name = "Seating ID")]
+        public long SEATING_ID { get; set; }
+    }
+}
---
+++ Gradiens/Gradiens/Models/HarborModels.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Gradiens.Models
+{
+    // Harbor object that is holds information for the HARBOR table
+    public class HarborModels
+    {
+        [Required]
+        [Display(Name = "Harbor ID")]
+        public long HARBOR_ID { get; set; }
+
+        [Required]
+        [Display(Name = "Harbor Name")]
+        public string HARBOR_NAME { get; set; }
+
+        [Required]
+        [Display(Name = "Harbor Location ID")]
+        public long HARBOR_LOCATION { get; set; }
+    }
+}
