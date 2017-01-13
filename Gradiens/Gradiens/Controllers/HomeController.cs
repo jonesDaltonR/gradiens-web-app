84a2cafa93e6af1cc848179b96a7a4916b05ac16
Joshua Estrada <jestrada5@email.davenport>
2017-01-13 15:59:44 -0500

    I changed the Message bag in the Home Controllers for the Contact page. I also changed the information on the contact page and the FAQ page to not be dalton's made up language.

--- Gradiens/Gradiens/Controllers/HomeController.cs
+++ Gradiens/Gradiens/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace Gradiens.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contact Us";
 
             return View();
         }
