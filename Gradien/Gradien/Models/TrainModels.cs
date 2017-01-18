ffe5d50eeeeb27b9423841eb3f25ac6126f83efa
Matthew Woodhead <mwoodhead@ad.davenport.edu>
2017-01-18 12:05:49 -0500

    Made changes to the models folder

---
+++ Gradien/Gradien/Models/TrainModels.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Gradiens.Models
+{
+    // Train object that is holds information for the TRAIN table
+    public class TrainModels
+    {
+        [Required]
+        [Display(Name = "Train ID")]
+        public long TRAIN_ID { get; set; }
+
+        [Required]
+        [Display(Name = "Train Company Name")]
+        public string TRAIN_COMPANY { get; set; }
+
+        [Required]
+        [Display(Name = "Station ID")]
+        public long STATION_ID { get; set; }
+
+        [Required]
+        [Display(Name = "Seating ID")]
+        public long SEATING_ID { get; set; }
+
+        public bool DELETE_STAT { get; set; }
+    }
+}
